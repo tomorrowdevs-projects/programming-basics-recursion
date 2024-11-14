cee730f2b24e6dc706a591b5d94aa380efc74d59
BloodYce <mattyago99@gmail.com>
2024-11-15 00:15:56 +0100

    Final Project Complete

--- projects/final-project/main.py
+++ projects/final-project/main.py
@@ -1,48 +1,48 @@
 '''
-# **Cataloger of Household Objects 🏡**
+# **Cataloger of Household elements 🏡**
 
-This project is to develop a household object cataloger, 
-a tool that helps users catalog and manage objects in their homes.   
-The cataloger will use recursion to explore home environments in depth, gathering information about objects, 
+This project is to develop a household element cataloger, 
+a tool that helps users catalog and manage elements in their homes.   
+The cataloger will use recursion to explore home environments in depth, gathering information about elements, 
 such as name, location, and category.   
-It will also provide functionality to search, add, edit, and delete objects from the catalog, 
+It will also provide functionality to search, add, edit, and delete elements from the catalog, 
 simplifying home inventory management.
 
 
-## **Objective 🎯**
+## **elementive 🎯**
 The goal of the project is to provide users with a practical tool for cataloging and keeping
-track of objects in their home.   
-Using variables, strings, mathematical calculations, conditional operators, loops, functions, arrays, JSON objects, 
-and recursive functions, the cataloger will enable users to effectively organize their household objects 
+track of elements in their home.   
+Using variables, strings, mathematical calculations, conditional operators, loops, functions, arrays, JSON elements, 
+and recursive functions, the cataloger will enable users to effectively organize their household elements 
 and obtain detailed information about them.
 
 ## **Recursive exploration of home environments 🔁**
 
-- Using a recursive function, explore home environments, such as rooms and closets, to identify the objects present.  
-- Gather information about objects, such as name, location, and category.
-- Use an appropriate data structure, such as an array of JSON objects, to store object information.
+- Using a recursive function, explore home environments, such as rooms and closets, to identify the elements present.  
+- Gather information about elements, such as name, location, and category.
+- Use an appropriate data structure, such as an array of JSON elements, to store element information.
 
-## **Adding new objects ➕**
+## **Adding new elements ➕**
 
-- Using a function, allow the user to add new objects to the catalog.
-- Capture the object's information, such as name, location, and category, and create a new JSON object with that information.
-- Add the object to the home object catalog.
+- Using a function, allow the user to add new elements to the catalog.
+- Capture the element's information, such as name, location, and category, and create a new JSON element with that information.
+- Add the element to the home element catalog.
 
-## **Modify existing objects ✍️**
+## **Modify existing elements ✍️**
 
-- Using a recursive function, allow the user to search and edit existing objects in the catalog.
-- Allow the user to edit the object's information, such as name, location, or category.
-- Update the corresponding JSON object in the catalog with the new information.
+- Using a recursive function, allow the user to search and edit existing elements in the catalog.
+- Allow the user to edit the element's information, such as name, location, or category.
+- Update the corresponding JSON element in the catalog with the new information.
 
 
-## **Object deletion ➖**
+## **element deletion ➖**
 
-- Using a recursive function, allow the user to search for and delete existing objects in the catalog.
-- Remove the corresponding JSON object from the home object catalog.
+- Using a recursive function, allow the user to search for and delete existing elements in the catalog.
+- Remove the corresponding JSON element from the home element catalog.
 
 ## **Limitations ✋**
 
-- The project focuses on cataloging and management of household objects, without including features such as object 
+- The project focuses on cataloging and management of household elements, without including features such as element 
   quantity management or integration with more complex inventory systems.
 - Interactions with external databases or other sources of additional information are not considered.
 
@@ -52,50 +52,65 @@ Considering the functionality required and the use of the specified constructs,
 the project is expected to take about 1 to 2 days to complete.
 '''
 
-def check_environment(catalog):
-    room = input("Please input which room do you want to explore: ")
-    for object in catalog:
-        if object["location"] == room:
-            print(object)
+def check_environment(catalog, index = 0, room = ""):
+    if catalog:
+        if index == 0:
+            room = input("Please input which room do you want to explore: ")
+        if catalog[index]["location"] == room:
+            print(catalog[index])
+            return catalog
+        index += 1
+        return check_environment(catalog, index, room)
+    return catalog
 
-def add_object(catalog):
-    name = input("Please input the name of the object: ")
-    location = input("Please input the position of the object: ")
-    category = input("Please input the category of the object: ")
-    object = name, location, category
-    catalog.append(object)
-    print("Object added correctly!")
+def add_element(catalog):
+    name = input("Please input the name of the element: ")
+    location = input("Please input the position of the element: ")
+    category = input("Please input the category of the element: ")
+    element = {"name": name,"location": location,"category": category}
+    catalog.append(element)
+    print("element added correctly!")
 
-def change_object(catalog):
-    name = input("Please input the name of which object you would like to modify?\n", catalog)
-    for object in catalog:
-        if object["name"] == name: 
-            object["location"] = input("Please input new location: ")
-            object["category"] = input("Please input new category: ")
-            print("Object modified correctly")
-            return
-    print("Object not found\n", catalog)
+def change_element(catalog, name):
+    if catalog:
+        if catalog[0]["name"] == name:
+            catalog[0]["name"] = input("Please input new name: ")
+            catalog[0]["location"] = input("Please input new location: ")
+            catalog[0]["category"] = input("Please input new category: ")
+            print("Element modified correctly")
+            return catalog
+        return change_element(catalog[1:], name)
+    print("Element not found\n", catalog)
+    return catalog
 
-def delete_object(catalog):
-    name = input("Please input the name of which object you would like to delete?\n", catalog)
-    for object in catalog:
-        if object["name"] == name: 
-            catalog.remove(object)
-            print("Object removed correctly")
-            return
-    print("Object not found\n", catalog)
+def delete_element(catalog, name, index = 0):
+    if catalog:
+        if catalog[index]["name"] == name:
+            del catalog[index]
+            print("Element deleted correctly")
+            return catalog
+        index += 1
+        return delete_element(catalog, name, index)
+    print("element not found\n", catalog)
+    return catalog
 
 def main():
-    catalog = []
-    userchoice = input("Please choose one of the following options\n 1. Search\n 2. Add\n 3. Edit\n 4. Delete")
-    if userchoice == "1":
-        check_environment(catalog)
-    elif userchoice == "2":
-        add_object(catalog)
-    elif userchoice == "3":
-        change_object(catalog)
-    elif userchoice == "4":
-        delete_object(catalog)
+    catalog = [{"name": "Fra", "location": "Living", "category": "Computer"}, 
+                {"name": "Mattia", "location": "Kitchen", "category": "Box"}, 
+                {"name": "Simone", "location": "Bathroom", "category": "Soap"}]
+    userchoice = input("Please choose one of the following options\n 1. Search\n 2. Add\n 3. Edit\n 4. Delete\n")
+    while userchoice != "":
+        if userchoice == "1":
+            check_environment(catalog)
+        elif userchoice == "2":
+            add_element(catalog)
+        elif userchoice == "3":
+            name = input("Please input the name of which element you would like to modify?\n")
+            change_element(catalog, name)
+        elif userchoice == "4":
+            name = input("Please input the name of which element you would like to delete?\n")
+            delete_element(catalog, name, index = 0)
+        userchoice = input("Please choose one of the following options\n 1. Search\n 2. Add\n 3. Edit\n 4. Delete\n")
 
 if __name__ == '__main__':
     main()
